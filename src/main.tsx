1892c6ec23af76050f8f0b5ccf5ceb9e15ec6b5d
RemiGirard <remi.girard@ntymail.com>
2023-05-07 14:27:49 +0200

    feat(ci): additional setup for build

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css";
 import Calcul from "./components/Calcul";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
