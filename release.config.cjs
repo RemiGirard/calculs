af83cea679da2176f3b1b4b6c4edbd023b3f87b2
RemiGirard <remi.girard@ntymail.com>
2023-05-08 10:59:26 +0200

    feat(ci): semantic

--- release.config.cjs
+++ release.config.cjs
@@ -8,7 +8,7 @@ const config = {
         'assets': ['dist/*.js', 'dist/*.js.map'],
         'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }],
-    '@sematic-release/github',
+    '@semantic-release/github',
   ]
 };
 
