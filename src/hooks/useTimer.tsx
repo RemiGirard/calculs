dd568bf8c5f03813ee1d705226bf83bdf28b7a1d
RemiGirard <remi.girard@ntymail.com>
2023-05-14 13:51:48 +0200

    feat(responsive):  adapt table to size and content

--- src/hooks/useTimer.tsx
+++ src/hooks/useTimer.tsx
@@ -1,25 +1,28 @@
 import { useState } from 'react';
 
-const useTimer = (duration = 10, callback = () => {console.log('default trigger')}, started = true, reset = false, setReset = (oneArgument: any) => {}) => {
-  const [time, setTime] = useState(duration)
-
-
-  if(started && !reset && time <= 0){
-    callback()
-    return time
-  }
+const useTimer = ({
+    duration = 10,
+    callback = () => {},
+    started = true,
+    setStarted = (oneArgument: any) => {},
+    reset = false,
+    setReset = (oneArgument: any) => {},
+  }) => {
+  const [time, setTime] = useState(duration);
 
   if(reset){
-    setTime(duration)
-    setReset(false)
-    return time 
+    setTime(duration);
+    setReset(false);
+  } else if (started  && time <= 0){
+    callback();
+    setStarted(false);
   } else if (started){
     setTimeout(() => {
       setTime(time - 1);
     }, 1000);  
   }
 
-  return time
+  return time;
 }
 
 export default useTimer;
--- src/routes/Game.tsx
+++ src/routes/Game.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import useTimer from '../hooks/useTimer';
 import ProgressBar from '../components/ProgressBar';
 import dictionaryTyped from '../dictionary.json'
+import Equation from '../components/Equation'
 
 const dictionary:any = dictionaryTyped;
 
@@ -11,13 +12,6 @@ const Game = ({exercices, setGameOver, config}: any) => {
     const [showResult, setShowResult] = useState(false)
     const rerollLimit = 1000;
 
-    const fontSize = 5
-
-    const colorsGradient = {
-        1: ['#ddcccc', '#66cc99'],
-        2: ['#ddcccc', '#bc66cc'],
-    }
-
     const backgroundColor = '#022b40';
 
     const [questionTimeStarted, setQuestionTimeStarted] = useState(true)
@@ -25,137 +19,138 @@ const Game = ({exercices, setGameOver, config}: any) => {
     const [answerTimeReset, setAnswerTimeReset] = useState(false)
     const [answerTimeStarted, setAnswerTimeStarted] = useState(false)
 
-    let [calculs, setCalculs]:[any, any] = useState([])
-
     function getRandomInt(min:any, max:any) {
-        return Math.round(Math.random() * (max - min))+ parseInt(min);
-    }
+      return Math.round(Math.random() * (max - min))+ parseInt(min);
+  }
 
-    const biggestNumberFirst = (first:any, second:any) => {
-        if(first < second){
-            [first, second] = [second, first]
-        }
-        return [first, second];
-    }
+  const biggestNumberFirst = (first:any, second:any) => {
+      if(first < second){
+          [first, second] = [second, first]
+      }
+      return [first, second];
+  }
 
-    function getRandomDivisibleNumbersInRange(min:any, max:any) {
+  function getRandomDivisibleNumbersInRange(min:any, max:any) {
 
-        let randomNumber = 0;
-        let isPrime = true;
+      let randomNumber = 0;
+      let isPrime = true;
 
-        // If the random number is prime, generate a new random number
-        // and check again until a non-prime number is found
-        while (isPrime) {
-            randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-            isPrime = true;
-            for (let i = 2; i < randomNumber; i++) {
+      // If the random number is prime, generate a new random number
+      // and check again until a non-prime number is found
+      while (isPrime) {
+          randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+          isPrime = true;
+          for (let i = 2; i < randomNumber; i++) {
             if (randomNumber % i === 0) {
                 isPrime = false;
                 break;
             }
-            }
-        }
-
-        // Generate a random divisor of the non-prime number
-        let randomDivisor = 0;
-        while (randomDivisor === 0 || randomNumber % randomDivisor !== 0 || (randomDivisor === 1 && (Math.random()> 0.2))) {
-            randomDivisor = Math.floor(Math.random() * (randomNumber - 1)) + 1;
-
-        }
-
-        // Return both the non-prime number and its random divisor in an array
-        return [randomNumber, randomDivisor];
+          }
       }
 
-      const numbersCombinaisonAlreadyGenerated = (numbers:any, calculLevel:any) => {
-        return !calculLevel.every((calculGroup:any) => calculGroup.every((curr:any) =>  JSON.stringify({...curr}) !== JSON.stringify({...numbers})) === true);
+      // Generate a random divisor of the non-prime number
+      let randomDivisor = 0;
+      while (randomDivisor === 0 || randomNumber % randomDivisor !== 0 || (randomDivisor === 1 && (Math.random()> 0.2))) {
+          randomDivisor = Math.floor(Math.random() * (randomNumber - 1)) + 1;
+
       }
 
-      const getGap = (gapType:any) => {
-        switch(gapType){
-            case 'result':
-                return 'result';
-            case 'firstElement':
-                return 1;
-            case 'secondElement':
-                return 2;
-            case 'randomOnTheTwoFirstElements':
-                return getRandomInt(1, 2);
-            case 'randomOnAll':
-                return getRandomInt(1, 3) === 3 ? 'result' : getRandomInt(1, 2);
-            default:
-                return 'result';
-        }
+      // Return both the non-prime number and its random divisor in an array
+      return [randomNumber, randomDivisor];
+    }
+
+    const numbersCombinaisonAlreadyGenerated = (numbers:any, calculLevel:any) => {
+      return !calculLevel.every((calculGroup:any) => calculGroup.every((curr:any) =>  JSON.stringify({...curr}) !== JSON.stringify({...numbers})) === true);
+    }
+
+    const getGap = (gapType:any) => {
+      switch(gapType){
+          case 'result':
+              return 'result';
+          case 'firstElement':
+              return 1;
+          case 'secondElement':
+              return 2;
+          case 'randomOnTheTwoFirstElements':
+              return getRandomInt(1, 2);
+          case 'randomOnAll':
+              return getRandomInt(1, 3) === 3 ? 'result' : getRandomInt(1, 2);
+          default:
+              return 'result';
+      }
+    }
+
+    const generateNumbers = ({group, limit, calculLevel, calculGroup, reroll=0}:any):any => {
+      let numbers:any = {
+          1: getRandomInt(group.min, group.max),
+          2: getRandomInt(group.min, group.max),
+          result: 0,
+          gap: getGap(limit.gap),
       }
 
-      const generateNumbers = ({group, limit, calculLevel, calculGroup, reroll=0}:any):any => {
-        let numbers:any = {
-            1: getRandomInt(group.min, group.max),
-            2: getRandomInt(group.min, group.max),
-            result: 0,
-            gap: getGap(limit.gap),
-        }
-
-        switch(limit.calcType){
-            case '+':
-                numbers.result = numbers[1]+numbers[2]
-                break;
-            case '+ x*10':
-                numbers[2]=(Math.max(10,Math.round(getRandomInt(limit.calcSpeRange.min, limit.calcSpeRange.max)/10)*10))
-                numbers.result = numbers[1]+numbers[2]
-                break;
-            case '+ x':
-                numbers[2]=limit.calcSpeNumber
-                numbers.result = numbers[1]+numbers[2]
-                break;
-            case '-':
-                [numbers[1], numbers[2]] = biggestNumberFirst(numbers[1], numbers[2])
-                numbers.result = numbers[1] - numbers[2]
-                break;
-            case '*':
-                numbers.result = numbers[1]*numbers[2]
-                break;
-            case '/':
-                [numbers[1], numbers[2]] = biggestNumberFirst(numbers[1], numbers[2])
-                numbers.result = parseFloat((numbers[1]/numbers[2]).toFixed(2))
-                break;
-            case '%':
-                [numbers[1], numbers[2]] = biggestNumberFirst(numbers[1], numbers[2])
-                numbers.result = [Math.floor(numbers[1]/numbers[2]), numbers[1]%numbers[2]]
-                break;
-            case '/ int':
-                [numbers[1], numbers[2]] = getRandomDivisibleNumbersInRange(group.min, group.max)
-                numbers.result = numbers[1]/numbers[2]
-                break;
-            default:
-        }
-
-        if(numbersCombinaisonAlreadyGenerated(numbers, [calculGroup, ...calculLevel]) && reroll < rerollLimit){
-            return generateNumbers({...{group, limit, calculLevel,calculGroup, reroll: reroll+1}})
-        } else {
-            return numbers;
-        }
+      switch(limit.calcType){
+          case '+':
+              numbers.result = numbers[1]+numbers[2]
+              break;
+          case '+ x*10':
+              numbers[2]=(Math.max(10,Math.round(getRandomInt(limit.calcSpeRange.min, limit.calcSpeRange.max)/10)*10))
+              numbers.result = numbers[1]+numbers[2]
+              break;
+          case '+ x':
+              numbers[2]=limit.calcSpeNumber
+              numbers.result = numbers[1]+numbers[2]
+              break;
+          case '-':
+              [numbers[1], numbers[2]] = biggestNumberFirst(numbers[1], numbers[2])
+              numbers.result = numbers[1] - numbers[2]
+              break;
+          case '*':
+              numbers.result = numbers[1]*numbers[2]
+              break;
+          case '/':
+              [numbers[1], numbers[2]] = biggestNumberFirst(numbers[1], numbers[2])
+              numbers.result = parseFloat((numbers[1]/numbers[2]).toFixed(2))
+              break;
+          case '%':
+              [numbers[1], numbers[2]] = biggestNumberFirst(numbers[1], numbers[2])
+              numbers.result = [Math.floor(numbers[1]/numbers[2]), numbers[1]%numbers[2]]
+              break;
+          case '/ int':
+              [numbers[1], numbers[2]] = getRandomDivisibleNumbersInRange(group.min, group.max)
+              numbers.result = numbers[1]/numbers[2]
+              break;
+          default:
       }
 
-      const generateCalcul = () => {
+      if(numbersCombinaisonAlreadyGenerated(numbers, [calculGroup, ...calculLevel]) && reroll < rerollLimit){
+          return generateNumbers({...{group, limit, calculLevel,calculGroup, reroll: reroll+1}})
+      } else {
+          return numbers;
+      }
+    }
+
+    const generateCalcul = () => {
         const newCalculs:any =[]
         exercices.forEach((limit:any) => {
           let calculLevel:any = [];
           limit.groups.forEach((group:any)=>{
             let calculGroup:any = []
             for(let i=0; i<limit.calcNumber;i++){
-                calculGroup.push(structuredClone(generateNumbers(structuredClone({group, limit, calculLevel, calculGroup}))))
+              calculGroup.push(structuredClone(generateNumbers(structuredClone({group, limit, calculLevel, calculGroup}))))
             }
             calculLevel.push(calculGroup)
           })
           newCalculs.push(calculLevel)
         });
-        setCalculs(newCalculs)
-      }
+        return newCalculs
+    }
 
-    useEffect(() => {
-        generateCalcul()
-    }, [])
+    const [calculs, setCalculs] = useState(generateCalcul())
+
+    
+
+      
+
 
     const handleReStartAnswerTime = () => {
         setAnswerTimeReset(true);
@@ -169,19 +164,6 @@ const Game = ({exercices, setGameOver, config}: any) => {
         setQuestionTimeStarted(true);
     }
 
-    function integerToLetter(integer:any) {
-      if (integer < 1 || integer > 26) {
-        return ' ';
-      }
-      
-      // Convert the integer to a character code by adding 64
-      var charCode = integer + 96;
-      
-      // Convert the character code to a letter
-      var letter = String.fromCharCode(charCode);
-      
-      return letter;
-    }
 
     const handleEndAnswerTime = () => {
         setAnswerTimeStarted(false);
@@ -199,76 +181,108 @@ const Game = ({exercices, setGameOver, config}: any) => {
         handleReStartAnswerTime()
     }
 
-    const getModuloResultDisplay = (quotient:any, remainder:any) => {
-        return (<div style={{display: 'flex', flexDirection: 'column'}}>
-            <div style={{fontSize: '0.5em'}}>quotient: {quotient}</div>
-            <div  style={{fontSize: '0.5em'}}>reste: {remainder}</div>
-        </div>)
-    }
-
-    const displayCalcElement = (currCalcul:any, element:any, showResult:any) => {
-      return (<div
-        style={{
-          width: element === 'result' && (exercices[currLevel].calcType === '%' || exercices[currLevel].calcType === '/')
-            ? '200px'
-            : '85px',
-          display: 'flex',
-          justifyContent: 'center',
-          color: currCalcul.gap === element && showResult ? '#00ff06' : '',
-          borderRadius: '15px',
-          padding: '5px',
-          marginLeft: '5px',
-        }}>
-        {currCalcul.gap !== element || showResult
-          ? <>
-              {
-                  element === 'result' && exercices[currLevel].calcType === '%'
-                      ? getModuloResultDisplay(currCalcul.result[0], currCalcul.result[1])
-                      : currCalcul[element]
-              }
-          </>
-          : <>..</>
-        }
-      </div>)
-    }
-    
-    const questionTime = useTimer(exercices[currLevel].questionDuration, () => {handleEndQuestionTime()}, questionTimeStarted, questionTimeReset, setQuestionTimeReset);
-    const answerTime = useTimer(exercices[currLevel].answerDuration, () => {handleEndAnswerTime()}, answerTimeStarted, answerTimeReset, setAnswerTimeReset);
+    const questionTime = useTimer({
+      duration: exercices[currLevel].questionDuration,
+      callback: handleEndQuestionTime,
+      started: questionTimeStarted,
+      reset: questionTimeReset,
+      setReset: setQuestionTimeReset,
+      });
+    const answerTime = useTimer({
+      duration: exercices[currLevel].answerDuration,
+      callback: handleEndAnswerTime,
+      started: answerTimeStarted,
+      reset: answerTimeReset,
+      setReset: setAnswerTimeReset,
+    });
 
     const barValue = questionTimeStarted
     ? (questionTime/exercices[currLevel].questionDuration)*100
     : ((exercices[currLevel].answerDuration-answerTime )/ exercices[currLevel].answerDuration) *100
-    return (<div style={{width: '100%', height: '100%', backgroundColor: backgroundColor, color: '#dddddd', fontFamily: 'arial-rounded-mt-bold', fontWeight: 'bold'}}>
-        {calculs.length
-            ? <div style={{width: '100%', height: '80%', display: 'flex', justifyContent: 'space-around', alignItems: 'center', fontSize: fontSize+'em'}}>
+
+    let granularity = [1, 1];
+    if (calculs && calculs[currLevel] && calculs[currLevel][0]){
+      granularity[1];
+    }
+
+    
+    const [maxTextLength, setMaxTextLength] = useState(1);
+
+    const updateParentTextSize = (number: number) => {
+      if(number > maxTextLength){
+        setMaxTextLength(number);
+      }
+    }
+
+    const updatedMaxTextLengthForListener = useRef(1);
+
+    useEffect(() => {
+      updatedMaxTextLengthForListener.current = maxTextLength;
+    }, [maxTextLength])
+
+    const calculsTable = useRef<HTMLElement | null>(null);
+
+    const [dynamicFontSize, setDynamicFontSize] = useState(1);
+
+    const calculateFontSize = () => {
+      if(calculsTable.current){
+        const widthSize = calculsTable.current.offsetWidth/calculs[currLevel].length;
+        const heightSize = calculsTable.current.offsetHeight/calculs[currLevel][0].length;
+        // maxTextLength
+        const textHeight = 1
+        setDynamicFontSize(Math.min(widthSize/(updatedMaxTextLengthForListener.current*12), heightSize/(textHeight*20)))
+      }
+    }
+    
+    useEffect(() => {
+      if(calculsTable && calculsTable.current){
+        calculateFontSize()
+        const widthObserver = new ResizeObserver(calculateFontSize);
+        widthObserver.observe(calculsTable.current)
+        return () => {
+          widthObserver.disconnect()
+        }
+      }
+    },[maxTextLength])
+
+
+    return (<div style={{width: '100%', height: '100%', backgroundColor: backgroundColor, color: '#dddddd',
+        fontFamily: 'arial-rounded-mt-bold', fontWeight: 'bold', display: 'flex', justifyContent: 'center'}}>
+            <div ref={calculsTable} style={{width: '95%', height: '85%', display: 'flex', justifyContent: 'space-around', alignItems: 'center', }}>
                 {
-                    calculs[currLevel].map((calculGroup:any, calculGroupIndex:any)=>{
-                        return <div key={calculGroupIndex}>
+                    exercices[currLevel].groups.map((exercice:any, exerciceIndex:any)=>{
+                        let groupSizes = 1;
+                        if(calculs && calculs[currLevel] && calculs[currLevel][exerciceIndex]){
+                          groupSizes = calculs[currLevel][exerciceIndex].reduce((acc: any, curr: any) => {
+                            let newAcc = structuredClone(acc);
+                            newAcc[1] = newAcc[1] < curr[1].toString().length ? curr[1].toString().length : newAcc[1];
+                            newAcc[2] = newAcc[2] < curr[2].toString().length ? curr[2].toString().length : newAcc[2];
+                            newAcc['result'] = newAcc['result'] < curr['result'].toString().length ? curr['result'].toString().length : newAcc['result'];
+                            return newAcc;
+                          }, {1: 0, 2: 0, result: 0});
+                        }
+                        return <div key={exerciceIndex} style={{display:'flex', flexDirection: 'column', width: (1/(calculs[currLevel].length)*100).toString()+'%', height: '100%', justifyContent: 'center'}}>
                             {
-                                calculGroup.map((currCalcul:any, index:any) => {
-                                  return (<div key={index} style={{display: 'flex', flexDirection: 'row', margin: '10px'}}>
-                                      {config.displayLetterId === true 
-                                        ? <div style={{color: '#0a4663', fontSize: '0.5em', display: 'flex', alignItems: "flex-end", marginBottom: '10px' }}>{integerToLetter(calculGroupIndex*(calculGroup.length) + (index+1))}. </div>
-                                        : null
-                                      }
-                                      <div style={{display: 'flex', flexDirection: 'row'}}>
-                                          {displayCalcElement(currCalcul, 1, showResult)}
-                                          {dictionary.calcCharacter[exercices[currLevel].calcType] ?? exercices[currLevel].calcType}
-                                          {displayCalcElement(currCalcul, 2, showResult)}
-                                      </div>
-                                      <div style={{display: 'flex', flexDirection: 'row'}}>
-                                        <> = </> 
-                                          {displayCalcElement(currCalcul, 'result', showResult)}
-                                          </div>
-                                  </div>)
+                                [...(new Array(exercices[currLevel].calcNumber))].map((currCalcul:any, index:any) => {
+                                  const calculGroup = calculs[currLevel][exerciceIndex];
+                                  return (calculs[currLevel][exerciceIndex][index] ? <Equation
+                                            key={index}
+                                            currCalcul={calculs[currLevel][exerciceIndex][index]}
+                                            index={index}
+                                            calculGroupIndex={exerciceIndex}
+                                            calculGroup={calculGroup}
+                                            config={config}
+                                            showResult={showResult} calcType={exercices[currLevel].calcType}
+                                            updateParentTextSize={updateParentTextSize}
+                                            groupSizes={groupSizes}
+                                            dynamicFontSize={dynamicFontSize}
+                                            />:null);
                                 })
                             }
                         </div>
                     })
                 }
             </div>
-            : null
-        }
         <div style={{display: 'flex', justifyContent: 'center',  width: '100%',position: 'absolute', bottom: '50px'}}>
           <div style={{width: '100%', height: '50px', display: 'flex', justifyContent: 'center'}}>
             <ProgressBar value={barValue}></ProgressBar>
