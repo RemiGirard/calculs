88e3c60aaf74f15f2e603bd3989a1f89f8ae7d73
RemiGirard <remi.girard@ntymail.com>
2023-05-14 22:19:33 +0200

    ci: add release rules

--- release.config.cjs
+++ release.config.cjs
@@ -1,7 +1,14 @@
 const config = {
   branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer', {
+      "preset": "angular",
+      "releaseRules": [
+        {"type": "docs", "scope":"README", "release": "patch"},
+        {"type": "refactor", "release": "patch"},
+        {"type": "style", "release": "patch"}
+      ],
+    }],
     '@semantic-release/release-notes-generator',
     ['@semantic-release/exec', {
       prepareCmd: 'mv dist/index.html dist/index-${nextRelease.version}.html'
