6c70e5963dfb9c9fbd497498cd10a6c66c70f20c
RemiGirard <remi.girard@protonmail.com>
2024-06-08 10:52:27 +0200

    build: add github action release, test, and page deployment

---
+++ release.config.cjs
@@ -0,0 +1,25 @@
+const config = {
+  branches: ['main'],
+  plugins: [
+    ['@semantic-release/commit-analyzer', {
+      "preset": "angular",
+      "releaseRules": [
+        {"type": "docs", "scope":"README", "release": "patch"},
+        {"type": "refactor", "release": "patch"},
+        {"type": "style", "release": "patch"}
+      ],
+    }],
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/exec', {
+      prepareCmd: 'mv dist/index.html dist/index-${nextRelease.version}.html'
+    }],
+    ['@semantic-release/github', {
+      'assets': [
+        {path: 'dist/index-*.html'}
+      ]
+    }],
+    '@semantic-release/git',
+  ]
+};
+
+module.exports = config;
