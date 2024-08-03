dd25c8d4c5db046fa330849730b1a318e91529e6
RemiGirard <remi.girard@protonmail.com>
2024-08-03 12:46:35 +0200

    fix: error on new exercise
    was due to a structured clone that convert object from class to classic object

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -17,7 +17,7 @@ export const getRandomItemOfArray = <T>(list: T[]): T|undefined => {
 export const getBodyRatio = () => document.body.clientWidth / document.body.clientHeight;
 
 export const shuffleArray = <T>(array: T[]) => {
-  const newArray = structuredClone(array);
+  const newArray = [...array];
   for (let i = newArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
