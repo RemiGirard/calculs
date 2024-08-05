2cb89c2857fdbddb3d30e5fa0063236a618ba521
RemiGirard <remi.girard@protonmail.com>
2024-08-05 11:36:33 +0200

    refactor: move App

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App.tsx';
+import App from './Presentation/App.tsx';
 import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
