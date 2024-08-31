d61b33543659dfcd2c73280ee64d38ba2c20b4a7
RemiGirard <remi.girard@protonmail.com>
2024-08-31 10:42:34 +0200

    fix: add tabindex to gap selector
    and keyboard interaction

--- src/Presentation/Molecules/Input/InputGap.tsx
+++ src/Presentation/Molecules/Input/InputGap.tsx
@@ -1,3 +1,5 @@
+import {KeyboardEvent} from "react";
+
 import EquationConfig from '@/Domain/GenerateExercises/Entity/EquationConfig.ts';
 import { setter } from '@/utils/type/setter.ts';
 import dictionary from "@/Presentation/dictionary.ts";
@@ -13,10 +15,19 @@ export default function ({ value, setValue }: componentProps) {
     const newValue = !value[key];
     setValue({ ...value, [key]: newValue });
   };
+
+  const onKeyDownHandler = (event:  KeyboardEvent<HTMLDivElement>, key: keyof EquationConfig['possibleGaps']) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      onChangeHandler(key);
+    }
+  }
+
   return (<InputGapWrapper>
       {(['first', 'second', 'result'] as const).map((key) => (<div
         key={key}
         onClick={() => onChangeHandler(key)}
+        onKeyDown={(event) => onKeyDownHandler(event, key)}
+        tabIndex={0}
       >
         <span>{value[key] ? '☑' : '☐'}</span> {dictionary.inputGapLabel[key]}
       </div>))}
